555c8ea487432fefd3446e788f8b88c80cd3a9ea
Anindita Guha <an711755@dal.ca>
2020-06-08 06:14:05 -0300

    Login Status feature added

--- app/model/DBConnector.py
+++ app/model/DBConnector.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import datetime
 
 
 
@@ -6,21 +7,57 @@ conn = mysql.connector.connect(host='database-2.ccmdqmbvy12j.us-east-1.rds.amazo
                                    ,user='admin',
                                    password='serverless',
                                    db='serverless')
-cursor = conn.cursor()
+cursor = conn.cursor(buffered=True)
 
 def SQLenterDetails(firstname,lastname,email,password):
     insertUser= "INSERT into userData(firstname,lastname,email,password) values(%s,%s,%s,%s)"
     cursor.execute(insertUser,(firstname,lastname,email,password))
     conn.commit()
 
-def CheckLoginCredentials(email,password):
 
-    findUser="Select password from userData where email=%s"
+
+def CheckLoginCredentials(email,password):
+    findUser="Select firstname,lastname,password from userData where email=%s"
     cursor.execute(findUser,(email,))
     results=cursor.fetchone()
-    if results[0]==password:
-        return True
+    if results[2]==password:
+        username=results[0]+' '+results[1]
+        return username
     else:
-        return False
+        return "no"
+
+def InsertintoUserState(email):
+    userlist=[]
+    status = "online"
+
+    findUser = "Select userId from userData where email=%s"
+    cursor.execute(findUser, (email,))
+    userid = cursor.fetchone()
+
+    checkonline="select * from userState where userId=%s"   #Check if user is already online
+    cursor.execute(checkonline,(userid[0],))
+    if cursor.rowcount==0:
+        insertOnlineTime="insert into userState (userId,status,onlineTime) values(%s,%s,%s)"
+        cursor.execute(insertOnlineTime, (userid[0],status, datetime.datetime.utcnow()))
+
+
+    # Find list of online users
+    findonlineusers="select userData.firstname, userData.lastname from userData natural join userState where userId in(select userId from userState where status=%s and userId!=%s)"
+    cursor.execute(findonlineusers,(status,userid[0]))
+    users=cursor.fetchall()
+    for i in range(len(users)):
+        userlist.append(users[i][0]+" "+ users[i][1])
+    conn.commit()
+    return userlist
+
+def logoutUser(username):
+    split_data=username.split()
+    logoutid="select userId from userData where firstname=%s and lastname=%s"
+    cursor.execute(logoutid,(split_data[1],split_data[2]))
+    conn.commit()
+    userid=cursor.fetchone()
+    print(userid)
+
+
 
 
--- app/routes/MainController.py
+++ app/routes/MainController.py
@@ -7,35 +7,44 @@ from app.service import MainService
 app = Flask(__name__, template_folder='../templates')
 
 
-
-@app.route('/register', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def registrationPage():
-
     # Checking validity of password and confirm password
-    if request.method== 'POST':
-        if request.form.get('password')!=request.form.get('confirmpassword'):
-                return render_template('registration.html', status="Passwords do not match")
+    if request.method == 'POST':
+        if request.form.get('password') != request.form.get('confirmpassword'):
+            return render_template('registration.html', status="Passwords do not match")
         else:
-            firstname= request.form.get('firstname')
+            firstname = request.form.get('firstname')
             lastname = request.form.get('lastname')
             email = request.form.get('email')
-            password= request.form.get('password')
-            DBConnector.SQLenterDetails(firstname,lastname,email,password)
+            password = request.form.get('password')
+            DBConnector.SQLenterDetails(firstname, lastname, email, password)
             return render_template('registration.html', status="Registration successful")
     return render_template('registration.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def loginPage():
-    #Check login credentials
-    if request.method=='POST':
-        email=request.form.get('email')
+    # Check login credentials
+    if request.method == 'POST':
+        email = request.form.get('email')
         password = request.form.get('password')
-        flag=DBConnector.CheckLoginCredentials(email, password)
-        if flag:
-            return render_template('login.html', status="Login Successful")
+        # find the username of email id
+        flag = DBConnector.CheckLoginCredentials(email, password)
+        print(flag)
+        if flag != "no":
+            list=DBConnector.InsertintoUserState(email)
+            return render_template('loginstatus.html', user=flag,list=list)
         else:
             return render_template('login.html', status="Invalid Credentials")
     return render_template('login.html')
+
+
+@app.route('/loginstatus',methods=['GET','POST'])
+def loginstatus():
+    if request.method=='POST':
+        DBConnector.logoutUser(request.form.get('username'))
+        return render_template('loginstatus.html')
 
 
 if __name__ == '__main__':
